f4247713a34102e25abc24356a585b0ab8a53dfe
houfang <707980114@qq.com>
2015-05-24 16:53:38 +0800

    侧边栏部分功能补充

--- app/src/main/java/com/example/administrator/carlighting/MainActivity.java
+++ app/src/main/java/com/example/administrator/carlighting/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.administrator.carlighting.mine.RentingRecordActivity;
+import com.example.administrator.carlighting.mine.WalletActivity;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 public class MainActivity extends Activity {
@@ -28,7 +30,7 @@ public class MainActivity extends Activity {
         SlidingMenu slidingMenu = new SlidingMenu(this);
         slidingMenu.setMode(SlidingMenu.LEFT);
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        slidingMenu.setBehindWidthRes(R.dimen.slidingmenu_offset);
         slidingMenu.setMenu(R.layout.slidingmenu);
         slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
     }
@@ -62,6 +64,36 @@ public class MainActivity extends Activity {
         startActivity(intent);
         overridePendingTransition(R.anim.push_right_in,
                 R.anim.push_right_out);
+    }
+
+    //todo 我的个人信息页面没有图片样式
+    public void click_to_info(View v) {
+
+    }
+
+    public void click_to_wallet(View v) {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        intent.setClass(this, WalletActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_right_in,
+                R.anim.push_right_out);
+    }
+
+    public void click_to_record(View v) {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        intent.setClass(this, RentingRecordActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_right_in,
+                R.anim.push_right_out);
+    }
+
+    //todo 环保贡献没有图片样式
+    public void click_to_contribute(View v) {
+
     }
 
 
---
+++ app/src/main/java/com/example/administrator/carlighting/mine/RentingRecordActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.carlighting.mine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.administrator.carlighting.R;
+
+/**
+ * Created by houfang on 15/5/24.
+ */
+public class RentingRecordActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pay_rente);
+    }
+}
